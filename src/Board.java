b8ba96cea4fcf9773260b6c1174b66e89bc1eb08
Faisal Alali <Faisalalali1@hotmail.com>
2022-01-29 18:08:40 +0300

    Added top & bottom boarders

--- src/Board.java
+++ src/Board.java
@@ -17,18 +17,48 @@ public class Board extends AnchorPane {
                 for (int y = 1; y < tiles.length; y++) {
                         double xCoordFront = (1) * (2 * (circumRadius * 0.75 + extendQ));
                         double yCoordFront = y * 2 * inRadius + ((offset ? 0 : 1) % 2) * inRadius
-                                        + (hieght / 2-1) * (2 * inRadius);
+                                        + (hieght / 2 - 1) * (2 * inRadius);
 
                         int lastCol = tiles[0].length - 1;
                         double xCoordBack = lastCol * 2 * (circumRadius * 0.75 + extendQ)
                                         + (width / 2 - 1) * (2 * (circumRadius * 0.75 + extendQ));
                         double yCoordBack = y * 2 * inRadius + ((lastCol + (offset ? 0 : 1)) % 2) *
-                                        inRadius + (hieght / 2-1) * (2 * inRadius);
+                                        inRadius + (hieght / 2 - 1) * (2 * inRadius);
                         tiles[y][0] = new HexagonTile(xCoordFront, yCoordFront, circumRadius, 'l',
                                         extendQ);
                         tiles[y][lastCol] = new HexagonTile(xCoordBack, yCoordBack, circumRadius,
                                         'r', extendQ);
                         getChildren().addAll(tiles[y][0], tiles[y][lastCol]);
+                }
+                for (int x = 1; x < tiles[0].length - 1; x++) {
+                        double xCoordTop = (x - 1) * 2 * (circumRadius * 0.75 + extendQ)
+                                        + (width / 2) * (2 * (circumRadius * 0.75 + extendQ));
+                        double yCoordTop = (-1) * 2 * inRadius + ((x + (offset ? 0 : 1)) % 2) * inRadius
+                                        + (hieght / 2) * (2 * inRadius);
+
+                        int lastRow = tiles.length - 1;
+                        double xCoordBot = (x - 1) * 2 * (circumRadius * 0.75 + extendQ)
+                                        + (width / 2) * (2 * (circumRadius * 0.75 + extendQ));
+                        double yCoordBot = (lastRow - 1) * 2 * inRadius + ((x + (offset ? 0 : 1)) % 2) * inRadius
+                                        + (hieght / 2) * (2 * inRadius);
+
+                        if (x == 1) {
+                                tiles[0][x] = new HexagonTile(xCoordTop, yCoordTop, circumRadius,
+                                                'w', extendQ);
+                                tiles[lastRow][x] = new HexagonTile(xCoordBot, yCoordBot, circumRadius,
+                                                'x', extendQ);
+                        } else if (x == lastRow - 1) {
+                                tiles[0][x] = new HexagonTile(xCoordTop, yCoordTop, circumRadius,
+                                                'y', extendQ);
+                                tiles[lastRow][x] = new HexagonTile(xCoordBot, yCoordBot, circumRadius,
+                                                'z', extendQ);
+                        } else {
+                                tiles[0][x] = new HexagonTile(xCoordTop, yCoordTop, circumRadius,
+                                                'o', extendQ);
+                                tiles[lastRow][x] = new HexagonTile(xCoordBot, yCoordBot, circumRadius,
+                                                'o', extendQ);
+                        }
+                        getChildren().addAll(tiles[0][x], tiles[lastRow][x]);
                 }
                 for (int y = 0; y < hieght; y++) {
                         for (int x = 0; x < width; x++) {
